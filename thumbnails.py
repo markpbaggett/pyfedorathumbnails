00ab8b6bfce6d3678cbee66f48f5b66f092c5342
Mark Baggett <mbagget1@utk.edu>
2015-06-19 17:41:31 -0400

    Modify python to allow users to create thumbnails for an entire collection

--- thumbnails.py
+++ thumbnails.py
@@ -3,21 +3,55 @@ from PIL import Image
 from StringIO import StringIO
 from optparse import OptionParser
 
-parser = OptionParser()
+def singleThumb():
+	global locationURL, userName, password, itemName, outputFile
+	locationURL += itemName + '/datastreams/TN/content?download=true'
+	r = requests.get(locationURL, auth=(userName, password))
+	if r.status_code == 200:
+		imageFile = StringIO(r.content)
+		img = Image.open(imageFile)
+		print 'Creating thumbnail for ' + itemName + ' as '+ outputFile + '. The format is ' + img.format + '.'
+		print r.status_code
+		img.save('temp/' + outputFile, "JPEG", quality=80, optimize=True, progressive=True)
+	else:
+		print 'Item does not exist.'
+		
+def collection():
+	global locationURL, userName, password, itemName, outputFile
+	attempts = 100
+	i = 1
+	totalObjects = 0
+	while i < attempts:
+		itemName = collectionName + '%3A' + `i`
+		newUrl = locationURL + itemName + '/datastreams/TN/content?download=true'
+		r = requests.get(newUrl, auth=(userName, password))
+		if r.status_code == 200:
+			imageFile = StringIO(r.content)
+			img = Image.open(imageFile)
+			img.save('temp/' + itemName + '.jpeg', "JPEG", quality=80, optimize=True, progressive=True)
+			totalObjects += 1
+		i += 1
+	print 'Created %d thumbnails' % totalObjects
+
 parser = OptionParser()
 parser.add_option("-u", "--user", dest="userName", help="submit username")
 parser.add_option("-p", "--password", dest="password", help="submit password")
 parser.add_option("-l", "--location", dest="locationURL", help="specify URL")
 parser.add_option("-i", "--itemName", dest="itemName", help="specify namespace of item")
 parser.add_option("-o", "--output", dest="outputFile", help="specify outputFileName")
+parser.add_option("-c", "--collection", dest="collectionName", help="specify a collection namespace")
 
 (options, args) = parser.parse_args()
-if options.userName is None or options.password is None or options.itemName is None:
-        parser.print_help()
-        parser.error("A username, password, and item is required!")
+if options.userName is None or options.password is None:
+    parser.print_help()
+    parser.error("A username, password, and item is required!")
+        
+if options.collectionName and options.itemName:
+    parser.print_help()
+    parser.error("Can only specify a collection or a item--not both!")	
 
 if options:
-	userName = password = locationURL = itemName = outputFile = ''
+	userName = password = locationURL = itemName = outputFile = collectionName = ''
 	if options.userName:
 		userName = options.userName
 	if options.password:
@@ -26,18 +60,14 @@ if options:
 		locationURL = options.locationURL
 	else:
 		locationURL = 'http://digital.lib.utk.edu:8080/fedora/objects/'
-	if options.itemName:
-		itemName = options.itemName
 	if options.outputFile:
 		outputFile = options.outputFile
 	else:
 		outputFile = 'output.jpeg'
+	if options.itemName:
+		itemName = options.itemName
+		singleThumb()
+	if options.collectionName:
+		collectionName = options.collectionName
+		collection()
 	
-locationURL += itemName + '/datastreams/TN/content?download=true'
-
-r = requests.get(locationURL, auth=(userName, password))
-
-imageFile = StringIO(r.content)
-img = Image.open(imageFile)
-print 'Creating thumbnail for ' + itemName + ' as '+ outputFile + '. The format is ' + img.format + '.'
-img.save('temp/' + outputFile, "JPEG", quality=80, optimize=True, progressive=True)
